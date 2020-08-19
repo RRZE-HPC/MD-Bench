0fd51e8a9cbc2cd18136207f4c8176f4881bd9bd
Jan Eitzinger <jan.eitzinger@fau.de>
2020-08-19 11:19:16 +0200

    Add OpenMP parallelization for computeForce.

--- include_CLANG.mk
+++ include_CLANG.mk
@@ -7,11 +7,11 @@ ANSI_CFLAGS += -std=c99
 ANSI_CFLAGS += -pedantic
 ANSI_CFLAGS += -Wextra
 
-CFLAGS   = -Ofast $(ANSI_CFLAGS) #-g
+CFLAGS   = -Ofast $(ANSI_CFLAGS) -Xpreprocessor -fopenmp #-g
 ASFLAGS  = -masm=intel
 CXXFLAGS = $(CFLAGS)
 FCFLAGS  =
 LFLAGS   =
 DEFINES  = -D_GNU_SOURCE -DALIGNMENT=64
 INCLUDES =
-LIBS     =
+LIBS     = -lomp
