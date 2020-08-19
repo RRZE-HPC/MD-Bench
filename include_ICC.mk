fb0ef3fa120f3258853cf991c9c96b4e58ed99b1
Jan Eitzinger <jan.eitzinger@fau.de>
2020-08-19 13:53:44 +0200

    Adapt ICC build config

--- include_ICC.mk
+++ include_ICC.mk
@@ -1,20 +1,19 @@
 CC  = icc
-CXX = mpiicpc
-FC  = ifort
-LINKER = $(CXX)
+CXX = icpc
+LINKER = $(CC)
 
 PROFILE  = #-g  -pg
-# OPTS     = -O3 -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
-# OPTS     =  -O3 -xCORE-AVX2  $(PROFILE)
-# OPTS     =  -O3 -xAVX  $(PROFILE)
-# OPTS     =  -O3 -xSSE4.2 $(PROFILE)
-OPTS     =  -O3 -no-vec $(PROFILE)
-# OPTS     =  -O3 -xHost $(PROFILE)
+OPTS     = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
+#OPTS     = -O3 -xCORE-AVX2  $(PROFILE)
+#OPTS     = -O3 -xAVX  $(PROFILE)
+#OPTS     = -O3 -xSSE4.2 $(PROFILE)
+#OPTS     = -O3 -no-vec $(PROFILE)
+#OPTS     =  -Ofast -xHost $(PROFILE)
 CFLAGS   = $(PROFILE) -restrict $(OPTS)
 CXXFLAGS = $(CFLAGS)
 ASFLAGS  = -masm=intel
 FCFLAGS  =
 LFLAGS   = $(PROFILE) $(OPTS)
-DEFINES  = -D_GNU_SOURCE  -DNOCHUNK -DUSE_SIMD# -DLIKWID_PERFMON   -DPRECISION=1
-INCLUDES = $(MPIINC) $(LIKWID_INC)
-LIBS     = $(LIKWID_LIB) -llikwid
+DEFINES  = -D_GNU_SOURCE  -DALIGNMENT=64 # -DLIKWID_PERFMON   -DPRECISION=1
+INCLUDES =
+LIBS     =
