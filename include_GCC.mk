32b23a2ebd48003d1986180cb6da9f20aab7387f
Jan Eitzinger <jan.eitzinger@fau.de>
2021-03-24 08:48:27 +0100

    Set defaults for GCC

--- include_GCC.mk
+++ include_GCC.mk
@@ -6,8 +6,8 @@ ANSI_CFLAGS += -std=c99
 ANSI_CFLAGS += -pedantic
 ANSI_CFLAGS += -Wextra
 
-CFLAGS   = -O0 -g  -std=c99 -fargument-noalias
-# CFLAGS   = -O3 -march=znver1  -ffast-math -funroll-loops -fopenmp
+# CFLAGS   = -O0 -g  -std=c99 -fargument-noalias
+CFLAGS   = -O3 -march=znver1  -ffast-math -funroll-loops # -fopenmp
 ASFLAGS  =  -masm=intel
 LFLAGS   =
 DEFINES  = -D_GNU_SOURCE
