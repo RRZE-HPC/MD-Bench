9d79f0248f271423b3cb26fab84f45ce98db5f30
Rafael Ravedutti <rafaelravedutti@gmail.com>
2021-08-09 21:37:18 +0200

    Add config option to switch between asm syntaxes
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- config.mk
+++ config.mk
@@ -1,18 +1,21 @@
-# Supported: GCC, CLANG, ICC
+# Compiler tag (GCC/CLANG/ICC)
 TAG ?= ICC
+# Enable likwid (true or false)
 ENABLE_LIKWID ?= false
 # SP or DP
 DATA_TYPE ?= DP
 # AOS or SOA
 DATA_LAYOUT ?= AOS
+# Assembly syntax to generate (ATT/INTEL)
+ASM_SYNTAX ?= INTEL
 
 # Number of times to run the neighbors loop on stubbed variant
 NEIGHBORS_LOOP_RUNS ?= 1
-# Explicitly store and load atom types
+# Explicitly store and load atom types (true or false)
 EXPLICIT_TYPES ?= false
-# Trace memory addresses for cache simulator
+# Trace memory addresses for cache simulator (true or false)
 MEM_TRACER ?= false
-# Trace indexes and distances for gather-md
+# Trace indexes and distances for gather-md (true or false)
 INDEX_TRACER ?= false
 # Vector width (elements) for index and distance tracer
 VECTOR_WIDTH ?= 8
