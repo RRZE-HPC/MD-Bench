ad047e92b743077ad1fae5242271945cf4f1d201
Maximilian Gaul <ptfs410h@testfront1.rrze.uni-erlangen.de>
2022-01-04 13:51:53 +0100

    Reminder for likwid perf measurements

--- evaluate_cpu_omp_perf.sh
+++ evaluate_cpu_omp_perf.sh
@@ -4,3 +4,7 @@ for i in $(seq 1 32); do
 	export "OMP_NUM_THREADS=$i"
 	./MDBench-GCC -n 50 | grep "Performance"
 done
+
+## likwid perf measurements on testfront1:
+# srun --nodes=1 --exclusive --nodelist=rome1 --time=00:30:00 --export=NONE -c 64 -C hwperf --pty /bin/bash -l
+# likwid-mpirun -np 1 -t 32 -m -g MEM -omp gnu -d ./MDBench-GCC
