ee4d7f250511a9bab79cf3ef8c71fd3814dddc9d
Rafael Ravedutti <rafaelravedutti@gmail.com>
2022-02-24 02:36:17 +0100

    Write function to read PDB files and include data for Argon simulation
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- gromacs/includes/atom.h
+++ gromacs/includes/atom.h
@@ -59,6 +59,8 @@ typedef struct {
 extern void initAtom(Atom*);
 extern void createAtom(Atom*, Parameter*);
 extern int readAtom(Atom*, Parameter*);
+extern int readAtom_pdb(Atom*, Parameter*);
+extern int readAtom_dmp(Atom*, Parameter*);
 extern void growAtom(Atom*);
 extern void growClusters(Atom*);
 
