5585ebcf425c361541b92c89c9461b82842d976e
Jan Eitzinger <jan.eitzinger@fau.de>
2022-04-01 15:57:54 +0200

    Add ONEAPI config. Remove omp simd for full neigh.

--- config.mk
+++ config.mk
@@ -1,19 +1,21 @@
-# Compiler tag (GCC/CLANG/ICC)
+# Compiler tag (GCC/CLANG/ICC/ONEAPI)
 TAG ?= ICC
 # Instruction set (SSE/AVX/AVX2/AVX512)
 ISA ?= AVX512
 # Optimization scheme (lammps/gromacs/clusters_per_bin)
-OPT_SCHEME ?= gromacs
+OPT_SCHEME ?= lammps
 # Enable likwid (true or false)
 ENABLE_LIKWID ?= true
 # SP or DP
-DATA_TYPE ?= SP
+DATA_TYPE ?= DP
 # AOS or SOA
 DATA_LAYOUT ?= AOS
 # Assembly syntax to generate (ATT/INTEL)
 ASM_SYNTAX ?= ATT
 # Debug
 DEBUG ?= false
+# Use omp simd pragma for lammps halfneigh
+ENABLE_OMP_SIMD ?= true
 
 # Explicitly store and load atom types (true or false)
 EXPLICIT_TYPES ?= false
@@ -22,7 +24,7 @@ MEM_TRACER ?= false
 # Trace indexes and distances for gather-md (true or false)
 INDEX_TRACER ?= false
 # Compute statistics
-COMPUTE_STATS ?= true
+COMPUTE_STATS ?= false
 
 # Configurations for gromacs optimization scheme
 # Use reference version
