3d2e689e7f990df6eca467655c0f0416e9a6c458
Jan Eitzinger <jan.eitzinger@fau.de>
2022-04-01 15:57:54 +0200

    Add ONEAPI config. Remove omp simd for full neigh.

--- include_ICC.mk
+++ include_ICC.mk
@@ -5,13 +5,13 @@ OPENMP  = #-qopenmp
 PROFILE  = #-profile-functions -g  -pg
 OPTS      = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
 #OPTS     = -Ofast -xCORE-AVX2  $(PROFILE)
-#OPTS     = -fast -xAVX  $(PROFILE)
-#OPTS     = -fast -xSSE4.2 $(PROFILE)
-#OPTS     = -fast -no-vec $(PROFILE)
-#OPTS     = -fast -xHost $(PROFILE)
+#OPTS     = -Ofast -xAVX  $(PROFILE)
+#OPTS     = -Ofast -xSSE4.2 $(PROFILE)
+#OPTS     = -Ofast -no-vec $(PROFILE)
+#OPTS     = -Ofast -xHost $(PROFILE)
 CFLAGS   = $(PROFILE) -restrict $(OPENMP) $(OPTS)
 ASFLAGS  = #-masm=intel
 LFLAGS   = $(PROFILE) $(OPTS) $(OPENMP)
-DEFINES  = -std=c11 -pedantic-errors -D_GNU_SOURCE #-DLIKWID_PERFMON
-INCLUDES = #$(LIKWID_INC)
-LIBS     = -lm #$(LIKWID_LIB) -llikwid
+DEFINES  = -std=c11 -pedantic-errors -D_GNU_SOURCE
+INCLUDES =
+LIBS     = -lm
