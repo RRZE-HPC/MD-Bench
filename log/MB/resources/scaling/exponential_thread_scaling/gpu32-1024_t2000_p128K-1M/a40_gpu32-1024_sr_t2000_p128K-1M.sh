292ba7b42bbddaa2733540a8d5edd97525e225b6
Martin Bauernfeind <martin.m.bauernfeind@fau.de>
2022-07-19 00:05:21 +0200

    Added measurements for scaling behaviour beyond 32 threads per block with different number of atoms

---
+++ log/MB/resources/scaling/exponential_thread_scaling/gpu32-1024_t2000_p128K-1M/a40_gpu32-1024_sr_t2000_p128K-1M.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -l
+#SBATCH --nodes=1 --ntasks=1 --time=01:00:00
+#SBATCH --export=NONE
+#SBATCH --gres=gpu:a40 --partition=a40
+unset SLURM_EXPORT_ENV
+
+module load likwid cuda		#load modules necessary for GPU profiling
+
+nx=32
+ny=32
+nz=32
+MAXTHREADS=1024
+GPU="a40"
+pSizeFancy="128K"
+
+all_output=""
+test_MD_Bench_scaling(){
+	part_filename=$GPU"_gpu32-1024_t2000_p"$pSizeFancy
+	all_output+="BEGIN Problem Size: $pSizeFancy"$'\n'
+	part_output=""
+	all_output+="nx:$nx ny:$ny nz:$nz"$'\n'
+	for ((tc=32;tc<=MAXTHREADS;tc*=2)); do #tc = threadcount per block
+	        output=$(eval "NUM_THREADS=$tc ~/MD-Bench/MDBench-NVCC -nx $nx -ny $ny -nz $nz -n 2000")
+		single_output=""
+		if [ "$part_output" != "" ]
+		then
+			single_output+=$'\n'
+		fi
+		single_output+=$(echo -n "$tc,")
+		single_output+=$(echo -n "$output" | grep 'atom updates per second' \
+			| sed 's/[^0-9.,]//g' | awk '{print $1"e6"}')
+		part_output+=$single_output
+		all_output+=$single_output
+	done
+	echo "$part_output"
+	echo "$part_output" > "$part_filename"
+	all_output+=$'\n'"END Problem Size: $pSizeFancy"$'\n'
+}
+
+test_MD_Bench_scaling
+nx=$((nx*2))
+pSizeFancy="256K"
+test_MD_Bench_scaling
+ny=$((ny*2))
+pSizeFancy="512K"
+test_MD_Bench_scaling
+nz=$((nz*2))
+pSizeFancy="1M"
+test_MD_Bench_scaling
+echo "$all_output" > "a40_cumulated"
---
+++ log/MB/resources/scaling/exponential_thread_scaling/gpu32-1024_t2000_p128K-1M/plot_a100.p
@@ -0,0 +1,19 @@
+set terminal png
+set output 'a100_gpu32-1024_t2000_pScaling.png'
+set title 'GPU threads-per-block scaling for one single GPU and its assigned cores'
+set xlabel '#GPU-Threads per block'
+set xrange [32:1024]
+set logscale x 2
+set yrange [0:*]
+set ylabel 'Atom updates [1/s]'
+set datafile separator ","
+set key bottom right
+set key samplen 1.5 spacing 1.1 font ",10"
+set style line 1 lc rgb '#0000ff' lt 1 lw 2 pt 7 ps 1.2
+set style line 2 lc rgb '#0080ff' lt 1 lw 2 pt 5 ps 1.2
+set style line 3 lc rgb '#00bfff' lt 1 lw 2 pt 9 ps 1.2
+set style line 4 lc rgb '#00ffbf' lt 1 lw 2 pt 13 ps 1.2
+plot 'a100_gpu32-1024_t2000_p128K' w lp ls 1 title '128Ki atoms', \
+        'a100_gpu32-1024_t2000_p256K' w lp ls 2 title '256Ki atoms', \
+        'a100_gpu32-1024_t2000_p512K' w lp ls 3 title '512Ki atoms', \
+        'a100_gpu32-1024_t2000_p1M' w lp ls 4 title '1Mi atoms'
