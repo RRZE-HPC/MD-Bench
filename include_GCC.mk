2e77f6207bfef08f1afefb2d52094c51428d4885
Rafael Ravedutti <rafaelravedutti@gmail.com>
2022-07-19 02:30:26 +0200

    Avoid errors when compiling for AVX2 due to SIMD LJ implementation
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- include_GCC.mk
+++ include_GCC.mk
@@ -8,10 +8,10 @@ ANSI_CFLAGS += -Wextra
 
 # CFLAGS   = -O0 -g  -std=c99 -fargument-noalias
 #CFLAGS   = -O3 -march=cascadelake  -ffast-math -funroll-loops # -fopenmp
-#CFLAGS   = -O3 -march=native  -ffast-math -funroll-loops # -fopenmp
-CFLAGS   = -O3 -march=znver1  -ffast-math -funroll-loops # -fopenmp
+CFLAGS   = -O3 -march=native  -ffast-math -funroll-loops # -fopenmp
+#CFLAGS   = -O3 -march=znver1  -ffast-math -funroll-loops # -fopenmp
 ASFLAGS  =  -masm=intel
 LFLAGS   =
-DEFINES  = -D_GNU_SOURCE -DLIKWID_PERFMON
+DEFINES  = -D_GNU_SOURCE -DNO_ZMM_INTRIN
 INCLUDES = $(LIKWID_INC)
-LIBS     = -lm $(LIKWID_LIB) -llikwid
+LIBS     = -lm
