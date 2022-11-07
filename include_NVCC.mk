437b3802291ae8294548615118fcd2d496a5d9ce
Rafael Ravedutti <rafaelravedutti@gmail.com>
2022-11-07 20:37:01 +0100

    Adjust NVCC flags
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- include_NVCC.mk
+++ include_NVCC.mk
@@ -6,9 +6,11 @@ ANSI_CFLAGS += -std=c99
 ANSI_CFLAGS += -pedantic
 ANSI_CFLAGS += -Wextra
 
-# CFLAGS   = -O0 -g  -std=c99 -fargument-noalias
+CFLAGS   = -O3 -march=native  -ffast-math -funroll-loops --forward-unknown-to-host-compiler # -fopenmp
+#CFLAGS   = -O3 -march=cascadelake  -ffast-math -funroll-loops --forward-unknown-to-host-compiler # -fopenmp
+#CFLAGS   = -O3 -g # -fopenmp
+#CFLAGS   = -O0 -g  -std=c99 -fargument-noalias
 #CFLAGS   = -O3 -g -arch=sm_61 # -fopenmp
-CFLAGS   = -O3 -g # -fopenmp
 ASFLAGS  =  -masm=intel
 LFLAGS   =
 DEFINES  = -D_GNU_SOURCE -DCUDA_TARGET -DNO_ZMM_INTRIN #-DLIKWID_PERFMON
