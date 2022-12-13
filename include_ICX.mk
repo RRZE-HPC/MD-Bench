d76d044a00118b15a954ae54064d6fc1131b1f6c
Rafael Ravedutti <rafaelravedutti@gmail.com>
2022-12-13 01:06:59 +0100

    Update build options for each compiler and include ICX
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- include_ICX.mk
+++ include_ICX.mk
@@ -1,18 +1,18 @@
-CC  = icc
+CC  = icx
 LINKER = $(CC)
 
 OPENMP  = #-qopenmp
 PROFILE  = #-profile-functions -g  -pg
-OPTS      = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
+#OPTS      = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
 #OPTS     = -Ofast -xCORE-AVX2  $(PROFILE)
 #OPTS     = -Ofast -xAVX  $(PROFILE)
 #OPTS     = -Ofast -xAVX2  $(PROFILE)
 #OPTS     = -Ofast -xSSE4.2 $(PROFILE)
 #OPTS     = -Ofast -no-vec $(PROFILE)
-#OPTS     = -Ofast -xHost $(PROFILE)
+OPTS     = -Ofast -xHost $(PROFILE)
 CFLAGS   = $(PROFILE) -restrict $(OPENMP) $(OPTS)
 ASFLAGS  = #-masm=intel
 LFLAGS   = $(PROFILE) $(OPTS) $(OPENMP)
-DEFINES  = -std=c11 -pedantic-errors -D_GNU_SOURCE
+DEFINES  = -std=c11 -pedantic-errors -D_GNU_SOURCE -DNO_ZMM_INTRIN
 INCLUDES =
 LIBS     = -lm
