b8d7501f57ef5808ba13886760bcfd7aa02aa5e8
Rafael Ravedutti <rafaelravedutti@gmail.com>
2022-12-14 17:54:18 +0100

    Adjust script for GROMACS scheme
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- util/evaluate_latency_and_cfd.sh
+++ util/evaluate_latency_and_cfd.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-MDBENCH_BIN=./MDBench-ICX-lammps
-FREQ=2.1
+TAG=ICX
+OPT_SCHEME=gromacs
+MDBENCH_BIN=./MDBench-$TAG-$OPT_SCHEME
+FREQ=2.4
 NRUNS=3
 FIXED_PARAMS=--freq $FREQ
+
+if [ "$OPT_SCHEME" = "gromacs" ]; then
+    STUB1_NAME=Stub-33
+    STUB1_PARAMS=-na 4 -nn 33
+    STUB2_NAME=Stub-128
+    STUB2_PARAMS=-na 4 -nn 128
+else
+    STUB1_NAME=Stub-76
+    STUB1_PARAMS=-nn 76
+    STUB2_NAME=Stub-1024
+    STUB2_PARAMS=-nn 1024
+fi
 
 function run_benchmark() {
     for i in $(seq $NRUNS); do
@@ -11,6 +25,8 @@ function run_benchmark() {
     done
 }
 
+echo "Tag: $TAG"
+echo "Optimization scheme: $OPT_SCHEME"
 echo "Binary: $MDBENCH_BIN(-stub)"
 echo "Frequency: $FREQ"
 echo "Number of runs: $NRUNS"
@@ -24,7 +40,7 @@ echo "Melt"
 run_benchmark $MDBENCH_BIN -i data/copper_melting/input_lj_cu_one_atomtype_20x20x20.dmp
 echo "Argon"
 run_benchmark $MDBENCH_BIN -p data/argon_1000/mdbench_params.conf -i data/argon_1000/tprout.gro
-echo "Stub-76"
-run_benchmark $MDBENCH_BIN-stub -nn 76
-echo "Stub-1024"
-run_benchmark $MDBENCH_BIN-stub -nn 1024
+echo "$STUB1_NAME"
+run_benchmark $MDBENCH_BIN-stub $STUB1_PARAMS
+echo "$STUB2_NAME"
+run_benchmark $MDBENCH_BIN-stub $STUB2_PARAMS
