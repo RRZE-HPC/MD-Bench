e5c233e072accd611b83c83f3e048777acbfafb1
Rafael Ravedutti <rafaelravedutti@gmail.com>
2022-12-21 18:04:18 +0100

    Update script
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- include_ICX.mk
+++ include_ICX.mk
@@ -3,13 +3,14 @@ LINKER = $(CC)
 
 OPENMP  = #-qopenmp
 PROFILE  = #-profile-functions -g  -pg
-#OPTS      = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
+OPTS      = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
+#OPTS      = -Ofast -march=cascadelake -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
 #OPTS     = -Ofast -xCORE-AVX2  $(PROFILE)
 #OPTS     = -Ofast -xAVX  $(PROFILE)
 #OPTS     = -Ofast -xAVX2  $(PROFILE)
 #OPTS     = -Ofast -xSSE4.2 $(PROFILE)
 #OPTS     = -Ofast -no-vec $(PROFILE)
-OPTS     = -Ofast -xHost $(PROFILE)
+#OPTS     = -Ofast -xHost $(PROFILE)
 CFLAGS   = $(PROFILE) $(OPENMP) $(OPTS)
 ASFLAGS  = #-masm=intel
 LFLAGS   = $(PROFILE) $(OPTS) $(OPENMP)
