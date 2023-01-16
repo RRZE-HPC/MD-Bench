ffad9d40f316fb7606d22539e38be434abfca5a0
Rafael Ravedutti <rafael.r.ravedutti@fau.de>
2023-01-16 22:45:24 +0100

    Use ICC compiler options based on ISA
    Signed-off-by: Rafael Ravedutti <rafael.r.ravedutti@fau.de>

--- include_ICC.mk
+++ include_ICC.mk
@@ -3,14 +3,27 @@ LINKER = $(CC)
 
 OPENMP  = #-qopenmp
 PROFILE  = #-profile-functions -g  -pg
+
+ifeq($(ISA),AVX512)
 OPTS      = -Ofast -xCORE-AVX512 -qopt-zmm-usage=high $(PROFILE)
-#OPTS     = -Ofast -xCORE-AVX2  $(PROFILE)
-#OPTS     = -Ofast -xAVX  $(PROFILE)
+endif
+
+ifeq($(ISA),AVX2)
+OPTS     = -Ofast -xCORE-AVX2  $(PROFILE)
 #OPTS     = -Ofast -xAVX2  $(PROFILE)
-#OPTS     = -Ofast -xSSE4.2 $(PROFILE)
+#OPTS     = -Ofast -march=core-avx2 $(PROFILE)
+endif
+
+ifeq($(ISA),AVX)
+OPTS     = -Ofast -xAVX  $(PROFILE)
+endif
+
+ifeq($(ISA),SSE)
+OPTS     = -Ofast -xSSE4.2 $(PROFILE)
+endif
+
 #OPTS     = -Ofast -no-vec $(PROFILE)
 #OPTS     = -Ofast -xHost $(PROFILE)
-#OPTS     = -Ofast -march=core-avx2 $(PROFILE)
 CFLAGS   = $(PROFILE) -restrict $(OPENMP) $(OPTS)
 ASFLAGS  = #-masm=intel
 LFLAGS   = $(PROFILE) $(OPTS) $(OPENMP)
