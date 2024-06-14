7727fcdc91554133edba75aee327e4829d5801d6
Jan Eitzinger <jan@moebiusband.org>
2024-06-14 09:31:13 +0200

    Revert to X86 and AVX2 as default

--- config.mk
+++ config.mk
@@ -1,11 +1,11 @@
 # Compiler tool chain (GCC/CLANG/ICC/ICX/ONEAPI/NVCC)
 TOOLCHAIN ?= CLANG
 # ISA of instruction code (X86/ARM)
-ISA ?= ARM
+ISA ?= X86
 # Instruction set for instrinsic kernels (NONE/<X86-SIMD>/<ARM-SIMD>)
 # with X86-SIMD options: NONE/SSE/AVX/AVX_FMA/AVX2/AVX512
 # with ARM-SIMD options: NONE/NEON/SVE/SVE2 (SVE not width-agnostic yet!)
-SIMD ?= NEON
+SIMD ?= AVX2
 # Optimization scheme (verletlist/clusterpair)
 OPT_SCHEME ?= verletlist
 # Enable likwid (true or false)
