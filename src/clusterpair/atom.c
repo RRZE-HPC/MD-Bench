3151562fc35f6f0e52298b913e18e76024314a5f
Rafael Ravedutti <rafaelravedutti@gmail.com>
2024-11-14 13:33:34 +0100

    Fix mask indexes for 4xn when M<N
    Signed-off-by: Rafael Ravedutti <rafaelravedutti@gmail.com>

--- src/clusterpair/atom.c
+++ src/clusterpair/atom.c
@@ -588,11 +588,11 @@ void initMasks(Atom* atom)
 
             atom->masks_4xn_fn[cond0 * 8 + cond1 * 4 + 0] = (unsigned int)(0x3 -
                                                                            0x1 * cond0);
-            atom->masks_4xn_fn[cond0 * 8 + cond1 * 4 + 0] = (unsigned int)(0x3 -
+            atom->masks_4xn_fn[cond0 * 8 + cond1 * 4 + 1] = (unsigned int)(0x3 -
                                                                            0x2 * cond0);
-            atom->masks_4xn_fn[cond0 * 8 + cond1 * 4 + 0] = (unsigned int)(0x3 -
+            atom->masks_4xn_fn[cond0 * 8 + cond1 * 4 + 2] = (unsigned int)(0x3 -
                                                                            0x1 * cond1);
-            atom->masks_4xn_fn[cond0 * 8 + cond1 * 4 + 0] = (unsigned int)(0x3 -
+            atom->masks_4xn_fn[cond0 * 8 + cond1 * 4 + 3] = (unsigned int)(0x3 -
                                                                            0x2 * cond1);
 #endif
         }
